b376d221ec162245803dadbb15f22633581a525b
Budi81921 <100458146+Budi81921@users.noreply.github.com>
2023-05-28 18:47:48 +0700

    Add files via upload

---
+++ Database/detail_pesanan.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2023 at 01:30 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan_uai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pesanan`
+--
+
+CREATE TABLE `detail_pesanan` (
+  `id_dp` tinyint(4) NOT NULL,
+  `DDC` int(11) NOT NULL,
+  `jumlah_pesanan` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD PRIMARY KEY (`id_dp`),
+  ADD UNIQUE KEY `id_dp` (`id_dp`),
+  ADD KEY `fk_dp_DDC` (`DDC`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  MODIFY `id_dp` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD CONSTRAINT `fk_dp_DDC` FOREIGN KEY (`DDC`) REFERENCES `koleksi` (`DDC`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Database/koleksi.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2023 at 01:31 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan_uai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `koleksi`
+--
+
+CREATE TABLE `koleksi` (
+  `DDC` int(11) NOT NULL,
+  `judul` varchar(200) NOT NULL,
+  `deskripsi_koleksi` tinytext NOT NULL DEFAULT '',
+  `jumlah_eksemplar` mediumint(9) NOT NULL DEFAULT 0,
+  `Penerbit` varchar(200) DEFAULT NULL,
+  `penulis` varchar(200) NOT NULL,
+  `status_koleksi` enum('Tersedia','Tidak Tersedia','Tersedia tetapi tidak bisa dipinjam') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `koleksi`
+--
+ALTER TABLE `koleksi`
+  ADD PRIMARY KEY (`DDC`),
+  ADD UNIQUE KEY `DDC` (`DDC`);
+ALTER TABLE `koleksi` ADD FULLTEXT KEY `koleks_search` (`judul`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
